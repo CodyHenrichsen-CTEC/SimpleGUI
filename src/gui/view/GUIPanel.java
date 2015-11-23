c0b91acab0ef2bbcadb62f7f595ea50b49747150
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2015-11-23 08:54:51 -0700

    Implemented MotionListener
    Added all methods required for MouseMotionListener interface

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -3,7 +3,7 @@
  */
 package gui.view;
 
-import javax.swing.*; 
+import javax.swing.*;
 import java.awt.event.*;
 import java.awt.Color;
 import gui.controller.GUIAppController;
@@ -54,15 +54,15 @@ public class GUIPanel extends JPanel
 		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 37, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.EAST, firstTextField, -24, SpringLayout.EAST, this);
 	}
-	
+
 	private void changeRandomColor()
 	{
 		int red, green, blue;
-		
+
 		red = (int) (Math.random() * 256);
 		green = (int) (Math.random() * 256);
 		blue = (int) (Math.random() * 256);
-	
+
 		this.setBackground(new Color(red, green, blue));
 	}
 
@@ -80,29 +80,47 @@ public class GUIPanel extends JPanel
 		{
 			public void mouseClicked(MouseEvent click)
 			{
-				
-			}
-			
-			public void mouseReleased(MouseEvent released)
-			{
-				
-			}
-			
-			public void mousePressed(MouseEvent pressed)
-			{
-				
-			}
-			
-			public void mouseEntered(MouseEvent entered)
-			{
-				
-			}
-			
-			public void mouseExited(MouseEvent exited)
-			{
-				
+
 			}
 
+			public void mouseReleased(MouseEvent released)
+			{
+
+			}
+
+			public void mousePressed(MouseEvent pressed)
+			{
+
+			}
+
+			public void mouseEntered(MouseEvent entered)
+			{
+				// changeRandomColor();
+			}
+
+			public void mouseExited(MouseEvent exited)
+			{
+				// changeRandomColor();
+			}
+
+		});
+
+		this.addMouseMotionListener(new MouseMotionListener()
+		{
+			public void mouseMoved(MouseEvent moved)
+			{
+				if(moved.isAltDown())
+				{
+					changeRandomColor();			
+				}
+				
+				
+			}
+			
+			public void mouseDragged(MouseEvent dragged)
+			{
+				
+			}
 		});
 	}
 
